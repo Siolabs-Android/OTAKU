e25c49717df9b9b88ed79388b38be06a9a961c31
Ashutosh Singh <dh_ashu@hotmail.com>
2014-09-02 19:12:35 +0530

    Login activity background change Adding new variables for the change dimensions and tyle Adding new drawable resources

--- src/com/example/otaku/Loginactivity.java
+++ src/com/example/otaku/Loginactivity.java
@@ -34,34 +34,34 @@ public class Loginactivity extends Activity {
 
 	// Code to login User
 	public void login(View v) {
-		User_table ut;
-		User_DAO ud = new User_DAO(this);
-		// Get the data from the editTexts
-		sf = this.getSharedPreferences("SP", Context.MODE_PRIVATE);
-		email = (EditText) findViewById(R.id.editText1);
-		password = (EditText) findViewById(R.id.editText2);
-		String emailID = email.getText().toString();
-		String pass = password.getText().toString();
-		ut = ud.getUser(emailID);
-		if (ut == null)
-			Toast.makeText(this, "This user deosn't exist!!",
-					Toast.LENGTH_SHORT).show();
-
-		else if (!ut.getPassword().equals(pass))
-			Toast.makeText(this, "You have entered a wrong password!!",
-					Toast.LENGTH_SHORT).show();
-
-		else {
-			int p=ut.get_id();
-			SharedPreferences.Editor editor = sf.edit();
-			editor.putString("userEmail", ut.getEmail());
-			editor.putInt("userId", ut.get_id());
-
-			editor.commit();
-
-			Intent i = new Intent(getApplicationContext(), Homepage.class);
-			startActivity(i);
-		}
+//		User_table ut;
+//		User_DAO ud = new User_DAO(this);
+//		// Get the data from the editTexts
+//		sf = this.getSharedPreferences("SP", Context.MODE_PRIVATE);
+//		email = (EditText) findViewById(R.id.editText1);
+//		password = (EditText) findViewById(R.id.editText2);
+//		String emailID = email.getText().toString();
+//		String pass = password.getText().toString();
+//		ut = ud.getUser(emailID);
+//		if (ut == null)
+//			Toast.makeText(this, "This user deosn't exist!!",
+//					Toast.LENGTH_SHORT).show();
+//
+//		else if (!ut.getPassword().equals(pass))
+//			Toast.makeText(this, "You have entered a wrong password!!",
+//					Toast.LENGTH_SHORT).show();
+//
+//		else {
+//			int p=ut.get_id();
+//			SharedPreferences.Editor editor = sf.edit();
+//			editor.putString("userEmail", ut.getEmail());
+//			editor.putInt("userId", ut.get_id());
+//
+//			editor.commit();
+//
+//			Intent i = new Intent(getApplicationContext(), Homepage.class);
+//			startActivity(i);
+//		}
 
 	}
 
