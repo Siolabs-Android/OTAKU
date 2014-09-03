125a22c4ec19df999471b0f14fd4a66f11f933f8
Ashutosh Singh <dh_ashu@hotmail.com>
2014-09-03 21:35:51 +0530

    reverting the toast show in the LoginAsyncTask

--- gen/com/example/otaku/R.java
+++ gen/com/example/otaku/R.java
@@ -10,42 +10,65 @@ package com.example.otaku;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
-        public static final int ic_action_refresh=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_drawer=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int search=0x7f020004;
+        public static final int background=0x7f020000;
+        public static final int button_default_bg=0x7f020001;
+        public static final int edittext_bottom_bg=0x7f020002;
+        public static final int edittext_default_bg=0x7f020003;
+        public static final int edittext_top_bg=0x7f020004;
+        public static final int email=0x7f020005;
+        public static final int ic_action_refresh=0x7f020006;
+        public static final int ic_action_search=0x7f020007;
+        public static final int ic_drawer=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int linearlayout_bg=0x7f02000a;
+        public static final int password=0x7f02000b;
+        public static final int search=0x7f02000c;
+        public static final int user=0x7f02000d;
     }
     public static final class id {
-        public static final int action_refresh=0x7f07001b;
-        public static final int action_search=0x7f07001a;
-        public static final int button1=0x7f070002;
-        public static final int button2=0x7f070003;
-        public static final int button3=0x7f07000e;
-        public static final int button4=0x7f070019;
-        public static final int drawer_layout=0x7f070015;
-        public static final int drawer_list=0x7f070017;
-        public static final int editText1=0x7f070005;
-        public static final int editText2=0x7f070008;
-        public static final int editText3=0x7f070001;
-        public static final int editText4=0x7f070011;
-        public static final int editText5=0x7f07000a;
-        public static final int editText6=0x7f07000c;
-        public static final int imageView1=0x7f070013;
-        public static final int imageView2=0x7f070018;
-        public static final int listView1=0x7f070006;
-        public static final int listView2=0x7f070014;
-        public static final int pager=0x7f070016;
-        public static final int scrollView1=0x7f07000d;
-        public static final int textView1=0x7f070004;
-        public static final int textView2=0x7f070007;
-        public static final int textView3=0x7f07000f;
-        public static final int textView4=0x7f070000;
-        public static final int textView5=0x7f070012;
-        public static final int textView6=0x7f070009;
-        public static final int textView7=0x7f07000b;
-        public static final int textView8=0x7f070010;
+        public static final int action_refresh=0x7f080023;
+        public static final int action_search=0x7f080022;
+        public static final int btnSignIn=0x7f08001d;
+        public static final int btnSignUp=0x7f08001e;
+        public static final int btnSingIn=0x7f08000a;
+        public static final int button1=0x7f080002;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f08000e;
+        public static final int button4=0x7f080020;
+        public static final int checkBox1=0x7f080021;
+        public static final int drawer_layout=0x7f080019;
+        public static final int drawer_list=0x7f08001b;
+        public static final int editText1=0x7f080005;
+        public static final int editText2=0x7f08000d;
+        public static final int editText3=0x7f080014;
+        public static final int editText4=0x7f080016;
+        public static final int editText5=0x7f080010;
+        public static final int editText6=0x7f080012;
+        public static final int etEmail=0x7f080007;
+        public static final int etName=0x7f080008;
+        public static final int etPass=0x7f080009;
+        public static final int etUserName=0x7f08001c;
+        public static final int imageView1=0x7f080001;
+        public static final int imageView2=0x7f08001f;
+        public static final int listView1=0x7f080006;
+        public static final int listView2=0x7f080018;
+        public static final int pager=0x7f08001a;
+        public static final int scrollView1=0x7f08000b;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f08000c;
+        public static final int textView3=0x7f080013;
+        public static final int textView4=0x7f080000;
+        public static final int textView5=0x7f080017;
+        public static final int textView6=0x7f08000f;
+        public static final int textView7=0x7f080011;
+        public static final int textView8=0x7f080015;
     }
     public static final class layout {
         public static final int activity_challengesubmission=0x7f030000;
@@ -56,45 +79,48 @@ public final class R {
         public static final int activity_friendlistforchallenge=0x7f030005;
         public static final int activity_friendsposts=0x7f030006;
         public static final int activity_homepage=0x7f030007;
-        public static final int activity_loginactivity=0x7f030008;
-        public static final int activity_myprofile=0x7f030009;
-        public static final int activity_publicprofile=0x7f03000a;
-        public static final int activity_searchresults=0x7f03000b;
-        public static final int activity_tasksubmission=0x7f03000c;
-        public static final int activity_topposts=0x7f03000d;
-        public static final int comment=0x7f03000e;
-        public static final int drawer_list_item=0x7f03000f;
-        public static final int friend=0x7f030010;
-        public static final int friendrequest=0x7f030011;
-        public static final int myperformedtasks=0x7f030012;
-        public static final int searchresult=0x7f030013;
-        public static final int selectfriend=0x7f030014;
+        public static final int activity_login_new=0x7f030008;
+        public static final int activity_loginactivity=0x7f030009;
+        public static final int activity_myprofile=0x7f03000a;
+        public static final int activity_publicprofile=0x7f03000b;
+        public static final int activity_searchresults=0x7f03000c;
+        public static final int activity_tasksubmission=0x7f03000d;
+        public static final int activity_topposts=0x7f03000e;
+        public static final int comment=0x7f03000f;
+        public static final int drawer_list_item=0x7f030010;
+        public static final int friend=0x7f030011;
+        public static final int friendrequest=0x7f030012;
+        public static final int myperformedtasks=0x7f030013;
+        public static final int searchresult=0x7f030014;
+        public static final int selectfriend=0x7f030015;
     }
     public static final class menu {
-        public static final int mainmenu1=0x7f060000;
+        public static final int mainmenu1=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int drawer_close=0x7f040014;
-        public static final int drawer_open=0x7f040013;
-        public static final int hello_world=0x7f040006;
-        public static final int title_activity_acceptchallenge=0x7f04000d;
-        public static final int title_activity_comments=0x7f040007;
-        public static final int title_activity_createaccount=0x7f040001;
-        public static final int title_activity_editprofile=0x7f040008;
-        public static final int title_activity_friendl_list=0x7f040010;
-        public static final int title_activity_friendlistforchallenge=0x7f04000f;
-        public static final int title_activity_friends=0x7f04000a;
-        public static final int title_activity_friendsposts=0x7f040011;
-        public static final int title_activity_homepage=0x7f040009;
-        public static final int title_activity_myprofile=0x7f04000c;
-        public static final int title_activity_publicprofile=0x7f040004;
-        public static final int title_activity_search=0x7f040003;
-        public static final int title_activity_search1=0x7f040005;
-        public static final int title_activity_searchresults=0x7f04000e;
-        public static final int title_activity_showposts=0x7f04000b;
-        public static final int title_activity_tasksubmission=0x7f040002;
-        public static final int title_activity_testfragment=0x7f040012;
+        public static final int action_settings=0x7f050016;
+        public static final int app_name=0x7f050000;
+        public static final int drawer_close=0x7f050014;
+        public static final int drawer_open=0x7f050013;
+        public static final int hello_world=0x7f050006;
+        public static final int title_activity_acceptchallenge=0x7f05000d;
+        public static final int title_activity_comments=0x7f050007;
+        public static final int title_activity_createaccount=0x7f050001;
+        public static final int title_activity_editprofile=0x7f050008;
+        public static final int title_activity_edittext1=0x7f050015;
+        public static final int title_activity_friendl_list=0x7f050010;
+        public static final int title_activity_friendlistforchallenge=0x7f05000f;
+        public static final int title_activity_friends=0x7f05000a;
+        public static final int title_activity_friendsposts=0x7f050011;
+        public static final int title_activity_homepage=0x7f050009;
+        public static final int title_activity_myprofile=0x7f05000c;
+        public static final int title_activity_publicprofile=0x7f050004;
+        public static final int title_activity_search=0x7f050003;
+        public static final int title_activity_search1=0x7f050005;
+        public static final int title_activity_searchresults=0x7f05000e;
+        public static final int title_activity_showposts=0x7f05000b;
+        public static final int title_activity_tasksubmission=0x7f050002;
+        public static final int title_activity_testfragment=0x7f050012;
     }
     public static final class style {
         /** 
@@ -118,10 +144,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060001;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060002;
+        public static final int DefaultButtonText=0x7f060000;
     }
 }
